73aba11a724df079b16a195f08f7837d4f12beb1
zerohh <81472909+cniu6@users.noreply.github.com>
2023-08-30 15:42:06 +0800

    first uplode 1.0
    first uplode 1.0

---
+++ cmd/main.py
@@ -0,0 +1,91 @@
+# -*- coding:utf-8 -*-
+# @Time   : 2023-08-30
+# @Author : cniu6 (zerohh)
+# @GitHub : https://github.com/cniu6/douyinpaging
+
+
+import pynput
+import time
+
+
+
+
+
+control = pynput.keyboard.Controller()
+mourol = pynput.mouse.Controller()
+
+
+def on_press(key):
+    # 按下按键时执行。
+    # 通过属性判断按键类型。
+    try:
+        print('字母数字  {0} 按下'.format(
+            key.char))
+    except AttributeError:
+        print('特殊的 {0} 按下'.format(
+            key))
+     
+
+
+def on_release(key):
+    # 松开按键时执行。
+    print('{0} 释放'.format(
+        key))
+
+    
+
+        # 如果监听是 page_up 就
+    if key == pynput.keyboard.Key.page_up:
+            time.sleep(0.5)
+            control.press(pynput.keyboard.Key.up)
+            time.sleep(0.01)
+            control.release(pynput.keyboard.Key.up)
+           
+
+         # 如果监听是 page_down 就
+    if key == pynput.keyboard.Key.page_down:
+            time.sleep(0.5)
+            control.press(pynput.keyboard.Key.down)
+            time.sleep(0.01)
+            control.release(pynput.keyboard.Key.down)
+            
+
+
+    # 如果监听是 "b" 就 在 0.2s内按下 alt+f3  快速关闭当前页面。  并且输出False退出程序。
+    if key == pynput.keyboard.KeyCode.from_char('b') or key == pynput.keyboard.KeyCode.from_char('B'):
+            control.press(pynput.keyboard.Key.alt_l)
+            control.press(pynput.keyboard.KeyCode.from_vk(115))
+            time.sleep(0.1)
+            control.release(pynput.keyboard.Key.alt_l)
+            control.release(pynput.keyboard.KeyCode.from_vk(115))
+            # Stop listener 
+            return False
+
+
+    if key == pynput.keyboard.Key.esc:
+        # Stop listener
+        return False
+
+
+print("=====================================")
+print("翻页笔抖音 - 使用翻页笔 上下模拟键盘上下键 滑动 抖音短视频,快手,TIKTOK,SHOUTS,基本通用。")
+print("by cniu6 (zerohh)       ")
+print("")
+print("仓库地址 https://github.com/cniu6/douyinpaging")
+print("=====================================")
+print("       ")
+print("1. 使用翻页笔 上下模拟鼠标滚轮 滑动 抖音短视频。    ")
+print("2. 翻页笔长按下键 <B>  快速关闭当前页面 并且 退出程序。   ")
+print("3. 键盘版。 -- 点击延迟0.5s     ")
+print("       ")
+print("=====================================")
+print("       ")
+print("       ")
+
+if __name__ == '__main__':
+    # 监听keyboard
+    with pynput.keyboard.Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
+        listener.join()
+
